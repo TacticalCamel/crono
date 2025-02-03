2065aa13f29fe5d19bee13828ed86e5ba8c94716
Takács Balázs <takacsbalazsg@gmail.com>
2025-02-03 15:13:23 +0100

    remove tests, remove eslint, change icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-/// <reference types="vitest" />
-
-import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue()
@@ -12,10 +9,6 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
-        },
-    },
-    test: {
-        globals: true,
-        environment: 'jsdom'
+        }
     }
 });
